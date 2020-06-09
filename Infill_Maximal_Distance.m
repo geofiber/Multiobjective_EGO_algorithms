22abfe91297febfd968e68daa6a4b9e7a72464ab
zhandawei <zhandawei@users.noreply.github.com>
2020-06-09 10:18:32 +0800

    update

--- Infill_Maximal_Distance.m
+++ Infill_Maximal_Distance.m
@@ -2,5 +2,5 @@ function obj = Infill_Maximal_Distance(x, sample_x)
 num_point = size(x,1);
 obj = zeros(num_point,1);
 for ii = 1 : num_point
-    obj(ii,:) = -min(sum((repmat(x(ii, :),size(sample_x),1) - sample_x).^2,2));
+    obj(ii,:) = -min(sum((repmat(x(ii, :),size(sample_x,1),1) - sample_x).^2,2));
 end
